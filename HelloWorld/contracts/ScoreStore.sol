f301a1e0fbe5567e7185b1edb43ebd26b593ec38
Taha Dalal <taha.dalal.6@gmail.com>
2022-08-10 18:38:03 +0530

    removing compiler warnings for ScoreStore

--- HelloWorld/contracts/ScoreStore.sol
+++ HelloWorld/contracts/ScoreStore.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ScoreStore{
@@ -7,7 +8,7 @@ contract ScoreStore{
         PersonScores[name] = score;
     }
 
-    function GetScore(string memory name) public returns (int){
+    function GetScore(string memory name) public view returns (int){
         return PersonScores[name];
     }
 }
--- HelloWorld/test/helloworld.js
+++ HelloWorld/test/helloworld.js
@@ -5,6 +5,6 @@ contract("HelloWorld", (accounts) => {
       const helloWorldInstance = await HelloWorld.deployed();
       const message = await helloWorldInstance.SayHello.call();
   
-      assert.equal(message.valueOf(), "Hello World !", "Incorrect message");
+      assert.equal(message.valueOf(), "Hello World !!", "Incorrect message returned by the function");
     });
   });
